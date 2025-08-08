8e146660bac3df4b4a67a6a5e1b0f70542d95642
thisisfatah <48764326+thisisfatah@users.noreply.github.com>
2025-08-08 16:03:45 +0700

    feat
    edited haptic example

--- Assets/Bumi Mobile Core/Examples/Example 03 - Haptic/Scripts/HapticTestController.cs
+++ Assets/Bumi Mobile Core/Examples/Example 03 - Haptic/Scripts/HapticTestController.cs
@@ -6,10 +6,43 @@ namespace BumiMobile
 {
     public class HapticTestController : MonoBehaviour
     {
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider durationSlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider intensitySlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Toggle advanceToggle;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Toggle dynamicToggle;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider minDelaySlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider minIntensitySlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider maxIntensitySlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider intensityStepSlider;
+        [BoxGroup("Reference", "Settings")]
+        [SerializeField]
+        private Slider intensityResetTimerSlider;
+        
         [SerializeField] HapticHandler longHapticHandler;
         
         [BoxGroup("Components", "Button")]
         [SerializeField] private Button backButton;
+        
+        private float lastPlayedTime = float.MinValue;
+
+        private int currentIntensityStep;
+        private float lastIntensityStepTime;
 
         private void Awake()
         {
@@ -21,32 +54,36 @@ namespace BumiMobile
 
         public void Play()
         {
-            Haptic.Play(0.3f);
-        }
+            if (advanceToggle.isOn)
+            {
+                if (Time.timeSinceLevelLoad < lastPlayedTime + minDelaySlider.value) return;
 
-        public void PlayLong()
-        {
-            longHapticHandler.Play();
-        }
+                lastPlayedTime = Time.timeSinceLevelLoad;
 
-        public void PlayLight()
-        {
-            Haptic.Play(Haptic.HAPTIC_LIGHT);
-        }
+                if (dynamicToggle.isOn)
+                {
+                    currentIntensityStep++;
 
-        public void PlayMedium()
-        {
-            Haptic.Play(Haptic.HAPTIC_MEDIUM);
-        }
+                    if (Time.timeSinceLevelLoad > lastIntensityStepTime)
+                        currentIntensityStep = 0;
 
-        public void PlayHard()
-        {
-            Haptic.Play(Haptic.HAPTIC_HARD);
-        }
+                    lastIntensityStepTime = Time.timeSinceLevelLoad + intensityResetTimerSlider.value;
 
-        public void PlayPattern()
-        {
-            Haptic.Play(Haptic.PATTERN_LIGHT);
+                    Haptic.Play(durationSlider.value, 
+                        Mathf.Lerp(
+                            minIntensitySlider.value, 
+                            maxIntensitySlider.value,
+                        (float)currentIntensityStep / intensityStepSlider.value));
+                }
+                else
+                {
+                    Haptic.Play(durationSlider.value, intensitySlider.value);
+                }
+            }
+            else
+            {
+                Haptic.Play(durationSlider.value, intensitySlider.value);
+            }
         }
     }
 }
